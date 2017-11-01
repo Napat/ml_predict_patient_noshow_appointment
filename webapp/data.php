2d5a22977026d3b268ae18910e0011caf2a8f8df
Apinya <apinya.pond@forth.co.th>
2017-11-01 10:45:50 +0700

    UPDATE: Add search condition to select patients by number of patients

--- webapp/data.php
+++ webapp/data.php
@@ -1,7 +1,23 @@
 <?php
 require_once("db.php");
-$data = db_get_first_200_patien();
+if($_POST["columnValue"] != "") {
+	//SELECT * FROM `patient` WHERE `PatientId` REGEXP '1' ORDER BY `ID` ASC LIMIT 0, 200
+	$sql = "SELECT * FROM patient ORDER BY ID ASC LIMIT 0, ".$_POST["columnValue"];
+	$conn = db_con();
+	$patien = $conn->query($sql, MYSQLI_USE_RESULT);
 
+	$patien_array = array();
+	// while( $row = $patien->fetch_array()){
+	while( $row = $patien->fetch_assoc()){
+		$patien_array[] = $row;
+	}
+	$patien->free();
+	
+	$data = $patien_array;
+}
+else {
+	$data = db_get_first_200_patien();
+}
 $json_object = new stdClass();
 $json_object->medical_data = $data;
 echo json_encode($json_object);
